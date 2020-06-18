2bbb76153d19a3dadd20c4a7eff1f8cc2c328b52
CleoMugs <cleophas.mugeni@gmail.com>
2020-06-18 11:13:01 +0300

    fixed flask-login typo

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
+from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
-from flask.ext.login import LoginManager
 from flask_pagedown import PageDown 
 from config import config
 
