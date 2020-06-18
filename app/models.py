de4054f02369c630a53cee7c04aac6053492288d
CleoMugs <cleophas.mugeni@gmail.com>
2020-06-18 11:49:15 +0300

    fixed db typo

--- app/models.py
+++ app/models.py
@@ -248,6 +248,46 @@ class Post(db.Model):
 	comments = db.relationship('Comment', backref='post', lazy='dynamic')
 
 
+	def to_json(self):
+		json_post = {
+			'url': url_for('api.get_post', id=self.id, _external=True),
+			'body': self.body,
+			'body_html': self.body_html,
+			'timestamp': self.timestamp,
+			'author': url_for('api.get_user', id=self.author_id,
+								_external=True),
+			'comments': url_for('api.get_post_comments', id=self.id,
+									_external=True),
+			'comment_count': self.comments.count()
+		}		
+		return json_post
+
+
+	def to_json(self):
+		json_user = {
+			'url': url_for('api.get_post', id=self.id, _external=True),
+			'username': self.username,
+			'member_since': self.member_since,
+			'last_seen': self.last_seen,
+			'posts': url_for('api.get_user_posts', id=self.id, _external=True),
+			'followed_posts': url_for('api.get_user_followed_posts',
+									  id=self.id, _external=True),
+			'post_count': self.posts.count()
+		}
+		return json_user
+
+
+
+	@staticmethod
+	def from_json(json_post):
+		body = json_post.get('body')
+		if body is None or body == '':
+			raise ValidationError('post does not have a body')
+		return Post(body=body)
+
+
+
+
 	@staticmethod
 	def generate_fake(count=100):
 		from random import seed, randint
