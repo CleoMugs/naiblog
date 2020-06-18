fdd756ced1c2a383d3f570299c92444af4152db7
CleoMugs <cleophas.mugeni@gmail.com>
2020-06-18 17:04:41 +0300

    fixed manage.py typo

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-	SSL_DISABLE = True
+	
 	SECRET_KEY = os.environ.get('SECRET_KEY') or '2gsu362gxbyat9jnx'
 	SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 	MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
@@ -15,11 +15,13 @@ class Config:
 	FLASKY_MAIL_SUBJECT_PREFIX = '[NaiBlog]'
 	FLASKY_MAIL_SENDER = 'NaiBlog Admin <flasky@example.com>'
 	FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+	#SSL_DISABLE = True
+	SSL_REDIRECT = False
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+	SQLALCHEMY_RECORD_QUERIES = True
 	FLASKY_POSTS_PER_PAGE = 4
 	FLASKY_FOLLOWERS_PER_PAGE = 5
 	FLASKY_COMMENTS_PER_PAGE = 5
-	SQLALCHEMY_TRACK_MODIFICATIONS = False
-	SQLALCHEMY_RECORD_QUERIES = True
 	FLASKY_SLOW_DB_QUERY_TIME = 0.5
 
 	@staticmethod
@@ -29,20 +31,23 @@ class Config:
 
 class DevelopmentConfig(Config):
 	DEBUG = True
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+		'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+'''
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	MAIL_SERVER = 'smtp.googlemail.com'
 	MAIL_PORT = 587
 	MAIL_USE_TLS = True
 	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-		'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
-
+'''	
 
 class TestingConfig(Config):
 	TESTING = True
 	SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
 		'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+	WTF_CSRF_ENABLED = False
 
 class ProductionConfig(Config):
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
@@ -74,8 +79,8 @@ class ProductionConfig(Config):
 		app.logger.addHandler(mail_handler)
 
 class HerokuConfig(ProductionConfig):
-	SSL_DISABLE = bool(os.environ.get('SSL_DISABLE'))
-	#SSL_REDIRECT = True if os.environ.get('DYNO') else False
+	#SSL_DISABLE = bool(os.environ.get('SSL_DISABLE'))
+	SSL_REDIRECT = True if os.environ.get('DYNO') else False
 	
 	@classmethod
 	def init_app(cls, app):
@@ -89,6 +94,7 @@ class HerokuConfig(ProductionConfig):
 		import logging
 		from logging import StreamHandler
 		file_handler = StreamHandler()
+		#file_handler.setLevel(logging.WARNING)
 		file_handler.setLevel(logging.WARNING)
 		app.logger.addHandler(file_handler)
 
--- manage.py
+++ manage.py
@@ -15,15 +15,17 @@ if os.environ.get('FLASK_COVERAGE'):
 
 
 import sys
-from app import create_app, db
-from app.models import User, Follow, Comment, Permission, Post, Role
+import click
+from flask_migrate import Migrate, upgrade, MigrateCommand
 from flask_script import Manager, Shell
-from flask_migrate import Migrate, MigrateCommand
+from app import create_app, db
+from app.models import User, Follow, Role, Permission, Post, Comment 
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
 manager = Manager(app)
 
+@app.shell_context_processor
 def make_shell_context():
 	return dict(app=app, db=db, User=User, Role=Role, 
 				Permission=Permission, Post=Post, Comment=Comment)
@@ -32,6 +34,8 @@ def make_shell_context():
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
+
+
 
 @manager.command
 def test(coverage=False):
